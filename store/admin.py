4d8ee48d9a03954e2aef2636f17cb81aee683e76
dimetylamin <tttd732000@gmail.com>
2022-05-06 14:02:25 +0700

    fisrt commit

--- store/admin.py
+++ store/admin.py
@@ -5,12 +5,16 @@ from .models import *
 # Register your models here.
 class Brand_display(admin.ModelAdmin):
     list_display=['name']
+    search_fields=['name']
 admin.site.register(Brand,Brand_display)
 
 class Product_display(admin.ModelAdmin):
     list_display=['name','brand','price','newprice','quantity','active']
+    search_fields=['name','brand__name']
+    list_filter=['brand__name']
 admin.site.register(Product, Product_display)
 
 class Order_display(admin.ModelAdmin):
     list_display=['name','phone','email','created_at','order_status']
+    search_fields=['name','phone','email']
 admin.site.register(Order,Order_display)
